12502fdec8f2e35e72b8132c3e20dd13cf6de1a9
nikhil sacena <nikhilhill19@gmail.com>
2023-10-24 18:41:03 +0530

    search-activated-user-side

--- src/admin/AdminLandingPage.js
+++ src/admin/AdminLandingPage.js
@@ -3,16 +3,16 @@ import "../user/UserLandingPage.css"
 import "./AdminLandingPage.css"
 import AdminNavBar from "./AdminNavBar"
 import UserNoteDisplay from "./UserNoteDisplay";
-export default function AdminLandingPage({ setAdminLoggedIn,notes,setNotes }) {
+export default function AdminLandingPage({ setAdminLoggedIn, notes, setNotes }) {
     // change to check on gitHub
     const users = ['Nikhil', 'User 2', 'User 3']
     const [activeUser, setActiveUser] = useState("Nikhil");
     const handleUserClick = (user) => {
-        if(user=="Nikhil"){
-        setUserWiseNotes(notes)
+        if (user == "Nikhil") {
+            setUserWiseNotes(notes)
 
         }
-        else{
+        else {
 
             setUserWiseNotes([])
         }
@@ -22,27 +22,27 @@ export default function AdminLandingPage({ setAdminLoggedIn,notes,setNotes }) {
         const updatedNotes = [...notes];
         updatedNotes.splice(index, 1);
         setNotes(updatedNotes);
-      };
-      const[userWiseNotes,setUserWiseNotes]=useState(notes)
+    };
+    const [userWiseNotes, setUserWiseNotes] = useState(notes)
     return (
         <>
             <AdminNavBar setAdminLoggedIn={setAdminLoggedIn} myTask="Admin" />
 
             <div className="admin-container">
-                <div style={{marginLeft:"10px",padding:"10px 10px 0px 10px",fontWeight:"500"}}>Select User</div>
+                <div style={{ marginLeft: "10px", padding: "10px 10px 0px 10px", fontWeight: "500" }}>Select User</div>
                 <ul>
-                    {users.map((user,index) => {
+                    {users.map((user, index) => {
                         return (
                             <li key={index}>
-                            <button style={{cursor:"pointer",width:"8em"}} className={`user-button ${activeUser === user ? 'active' : ''}`}
-                                onClick={() => handleUserClick(user)}>{user}</button>
-                        </li>
+                                <button style={{ cursor: "pointer", width: "8em" }} className={`user-button ${activeUser === user ? 'active' : ''}`}
+                                    onClick={() => handleUserClick(user)}>{user}</button>
+                            </li>
                         )
                     })}
 
                 </ul>
 
-               <UserNoteDisplay userWiseNotes={userWiseNotes} handleDeleteNote={handleDeleteNote}/>
+                <UserNoteDisplay userWiseNotes={userWiseNotes} handleDeleteNote={handleDeleteNote} />
             </div>
         </>
 
--- src/user/UserLandingPage.js
+++ src/user/UserLandingPage.js
@@ -1,23 +1,26 @@
 import AdminNavBar from "../admin/AdminNavBar"
-import React from 'react';
+import React, { useState } from 'react';
 
 import NoteAddModal from "./NoteAddModal";
 import "./UserLandingPage.css"
-export default function UserLandingPage({ setUserLoggedIn,note,setNote,setNotes,notes }) {
+export default function UserLandingPage({ setUserLoggedIn, note, setNote, setNotes, notes }) {
+    const [searchTerm, setSearchTerm] = useState('');
     const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-    const aray = ['bnmnllkjlk', 'hbhkjhbjk']
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     const handleDeleteNote = (index) => {
         const updatedNotes = [...notes];
         updatedNotes.splice(index, 1);
         setNotes(updatedNotes);
-      };
+    };
+    const filteredNotes = notes.filter((note) =>
+        note.toLowerCase().includes(searchTerm.toLowerCase())
+    );
     return (
         <>
             <AdminNavBar setAdminLoggedIn={setUserLoggedIn} myTask="Nikhil" />
             <div className="user-container">
-                <div style={{ fontSize: "1.5em", fontWeight: 500,padding:"10px" }}>Notes</div>
+                <div style={{ fontSize: "1.5em", fontWeight: 500, padding: "10px" }}>Notes</div>
 
                 <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                     <input
@@ -25,29 +28,37 @@ export default function UserLandingPage({ setUserLoggedIn,note,setNote,setNotes,
                         placeholder="Search"
                         className="search-user"
                         style={{ backgroundColor: "#f2f2f2", width: "90%" }}
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
                     />
-                    <button className="search-button">Search</button>
+                    {/* <button className="search-button">Search</button> */}
                 </div>
                 <div className="add-note">
                     <button className="search-button add-button" onClick={handleOpen}>Add Note</button>
                 </div>
-               
-                    {notes.map((para,index) => {
+                {/* <hr></hr> */}
+                <ul>
+                    {filteredNotes.map((para, index) => {
                         return (
-                            <div className="note-container">
-                                <div className="note">
-                                    {para}
+                            <li>
+                                <div className="note-container" key={index}>
+                                    <div className="note">
+                                        {para}
+                                    </div>
+                                    <input type="checkbox" id="myCheckbox" />
+                                    <button className="search-button" onClick={() => handleDeleteNote(index)} >Delete</button>
                                 </div>
-                                <input type="checkbox" id="myCheckbox"  />
-                                <button className="search-button"  onClick={() => handleDeleteNote(index)} >Delete</button>
-                            </div>
-                            
+                                <hr></hr>
+                            </li>
+
                         )
                     })}
+                </ul>
 
-             
+
+
             </div>
-                <NoteAddModal open={open} setOpen={setOpen} handleClose={handleClose} note={note} setNote={setNote} setNotes={setNotes}notes={notes}/>
+            <NoteAddModal open={open} setOpen={setOpen} handleClose={handleClose} note={note} setNote={setNote} setNotes={setNotes} notes={notes} />
 
         </>
     )
